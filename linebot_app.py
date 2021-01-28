acb238a6a545a506ad14b67fd02f4696e104240a
newsun87 <newsun87@mail.sju.edu.tw>
2021-01-28 12:43:37 +0000

    init.

--- linebot_app.py
+++ linebot_app.py
@@ -76,7 +76,8 @@ def uploadfile_gdrive(filepath, filename):
     if gfile.uploaded:
       os.remove(filepath)
       result = '檔案傳送完成...'
-      client.publish("cups/cups0001", "print", qos=1)              
+      client.publish("cups/cups0001", "print", qos=1)
+      client.publish("cups/cups0001", "", qos=1)               
   except:
     print("Uploading failed.")
     result = '檔案傳送失敗...'
