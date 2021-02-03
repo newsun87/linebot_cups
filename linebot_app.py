58918a05c7ba266915d17842cc7eaaa5bf568fc7
newsun87 <newsun87@mail.sju.edu.tw>
2021-02-03 05:31:54 +0000

    init.

--- linebot_app.py
+++ linebot_app.py
@@ -248,6 +248,6 @@ client.loop_start()
 
 if __name__ == "__main__":
   
-  app.run(debug=True, host='0.0.0.0', port=5000)
+  app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
 
      
